63c42280fe852d05758c519c1a6e321f90674d84
songhee <20150261sh@gmail.com>
2020-11-26 14:55:58 +0900

    11/25

---
+++ Algorithm/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  백준 알고리즘
+//
+//  Created by songhee on 2020/11/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << 10/4;
+    return 0;
+}
