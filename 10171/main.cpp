de698b32e7412d0031f6363507c066350693a359
songhee <songhi1115@gmail.com>
2020-12-30 13:39:36 +0900

    1단계 입출력과 사칙연산 complete

---
+++ 10171/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  10171
+//
+//  Created by songhee on 2020/12/30.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
