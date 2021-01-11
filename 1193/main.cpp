6cbc718bfbbb58ee08a036135b811b5aab7d8b66
songhee <songhi1115@gmail.com>
2021-01-11 20:59:31 +0900

    8단계 기본 수학1 5/9 진행중

--- 1193/main.cpp
+++ 1193/main.cpp
@@ -8,7 +8,40 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int N, a = 1, r = 1, sum = 2, x, y;
+    scanf("%d", &N);
+    if (N == 1)
+    {
+        printf("1/1\n");
+        return 0;
+    }
+    while(1)
+    {
+        a += r;
+        r++;
+        sum++;
+        if(a <= N && N < a+r) break;
+    }
+    if(sum % 2 == 0)
+    {
+        x = sum;
+        for(int i = a; i < a+r; i++)
+        {
+            x--;
+            y = sum-x;
+            if(i == N) break;
+        }
+    }
+    else
+    {
+        x = 0;
+        for(int i = a; i < a+r; i++)
+        {
+            x++;
+            y = sum-x;
+            if(i == N) break;
+        }
+    }
+    printf("%d/%d\n", x, y);
     return 0;
 }
