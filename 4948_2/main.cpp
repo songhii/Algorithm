218b2bf0d456d966fda8bfef557806926f6c3482
songhee <songhi1115@gmail.com>
2021-01-15 13:49:48 +0900

    9단계 기본 수학2 베르트랑 공준 (에라토스테네스의 체 적용)

---
+++ 4948_2/main.cpp
@@ -0,0 +1,38 @@
+//
+//  main.cpp
+//  4948_2
+//
+//  Created by songhee on 2021/01/15.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int p[246913];
+    fill_n(p, 246913, 1);
+    p[0] = 0; p[1] = 0;
+    int n;
+    
+    for(int i = 2; i * i <= 246913; i++)
+    {
+        if(p[i] == 0) continue;
+        for(int j = i+i; j <= 246913; j+=i)
+        {
+            p[j] = 0;
+        }
+    }
+    while(1)
+    {
+        scanf("%d", &n);
+        if(n == 0) return 0;
+        int sum = 0;
+        for(int i = n+1; i <= 2*n; i++)
+        {
+            if(p[i] == 1) sum++;
+        }
+        printf("%d\n", sum);
+    }
+    
+    return 0;
+}
