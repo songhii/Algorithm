6f620d9c47548dd51868685f9b9662a30c79bd22
songhee <songhi1115@gmail.com>
2021-01-25 18:06:29 +0900

    좌표 정렬하기2 - sort 콜백 함수 사용

--- 11651/main.cpp
+++ 11651/main.cpp
@@ -6,9 +6,29 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool compare(pair<int, int> v1, pair<int, int> v2)
+{
+    if(v1.second == v2.second)
+        return v1.first < v2.first;
+    return v1.second < v2.second;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int N; scanf("%d", &N);
+    vector<pair<int, int>> v;
+    for(int i = 0; i < N; i++)
+    {
+        int x, y; scanf("%d %d", &x, &y);
+        v.push_back({x, y});
+    }
+    sort(v.begin(), v.end(), compare);
+    for(int i = 0; i < N; i++)
+    {
+        printf("%d %d\n", v[i].first, v[i].second);
+    }
     return 0;
 }
