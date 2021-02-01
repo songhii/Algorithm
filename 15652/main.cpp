2b8bd54382c698ef5e4c72054c9d6c1def73fccb
songhee <songhi1115@gmail.com>
2021-02-01 13:00:43 +0900

    N과 M(4) - dfs 사용

---
+++ 15652/main.cpp
@@ -0,0 +1,40 @@
+//
+//  main.cpp
+//  15652
+//
+//  Created by songhee on 2021/02/01.
+//
+
+#include <iostream>
+using namespace std;
+
+#define MAX 8+1
+int N, M;
+int arr[MAX];
+
+void dfs(int x)
+{
+    if(x == M)
+    {
+        for(int i = 0; i < M; i++)
+        printf("%d ", arr[i]);
+        printf("\n");
+        return;
+    }
+    
+    for(int i = 1; i <= N; i++)
+    {
+        if(arr[x-1] <= i)
+        {
+            arr[x] = i;
+            dfs(x+1);
+            
+        }
+    }
+}
+
+int main(int argc, const char * argv[]) {
+    scanf("%d %d", &N, &M);
+    dfs(0);
+    return 0;
+}
