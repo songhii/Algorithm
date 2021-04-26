04f237d484a9670be26c4cb3373f18ff158158b2
songhee <songhi1115@gmail.com>
2021-04-26 14:49:40 +0900

    돌 게임

---
+++ 9655/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  9655
+//
+//  Created by songhee on 2021/04/26.
+//
+
+#include <iostream>
+using namespace std;
+
+int N, SK = 0, CY = 0;
+string flag = "SK";
+
+int main(int argc, const char * argv[]) {
+    scanf("%d", &N);
+    if(N % 2 == 0) cout << "CY" << endl;
+    else cout << "SK" << endl;
+}
